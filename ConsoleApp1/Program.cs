c14f0341de1471d623212b7defe84d5322eb424f
morgunovdv <morgunovdv3@hotmail.com>
2021-03-23 21:20:17 +0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет. Меня зовут Денис.");
+            Console.WriteLine("А как зовут тебя?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Приятно познакомиться {name}");
+        }
+    }
+}
